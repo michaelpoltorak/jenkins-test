0cc70ef726e83314a897d8e27b2b5ab1e8cae526
michaelpoltorak <michaelpoltorak@gmail.com>
2015-09-26 00:32:50 +0200

    hello world 6

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello world 5');
+  res.send('hello world 6');
 });
 
 app.listen(process.env.PORT || 5000);
-console.log("Action at port 5000");
+//console.log("Action at port 5000");
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world 5', done);
+    request(app).get('/').expect('hello world 6', done);
   });
 });
